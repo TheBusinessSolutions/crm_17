feb84d2906876e7f910fa8516ed777d059f349bb
alihassanmostafa <alihassan.mostafa@gmail.com>
2026-03-02 16:10:42 +0200

    add function to create a new quotation for selected products

--- crm_lead_product/models/crm_lead.py
+++ crm_lead_product/models/crm_lead.py
@@ -1,14 +1,17 @@
 # Copyright (C) 2017-2024 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from odoo import api, fields, models
+from odoo import api, fields, models, Command, _
+from odoo.exceptions import UserError
 
 
 class CrmLead(models.Model):
     _inherit = "crm.lead"
 
     lead_line_ids = fields.One2many(
-        comodel_name="crm.lead.line", inverse_name="lead_id", string="Lead Lines"
+        comodel_name="crm.lead.line", 
+        inverse_name="lead_id", 
+        string="Lead Lines"
     )
 
     @api.onchange("lead_line_ids")
@@ -25,3 +28,46 @@ class CrmLead(models.Model):
             expected_revenue += lead_line.expected_revenue
         res["expected_revenue"] = expected_revenue
         return res
+
+    def action_make_quotation(self):
+        """
+        Creates a Sale Order based on the Lead Lines and redirects to it.
+        """
+        self.ensure_one()
+        if not self.partner_id:
+            raise UserError(_("Please set a customer before creating a quotation."))
+
+        if not self.lead_line_ids:
+            raise UserError(_("There are no product lines to convert into a quotation."))
+
+        # Prepare Order Lines using Command.create for Odoo 17
+        order_lines = []
+        for line in self.lead_line_ids:
+            order_lines.append(Command.create({
+                'product_id': line.product_id.id,
+                'name': line.name,
+                'product_uom_qty': line.product_qty,
+                'product_uom': line.uom_id.id,
+                'price_unit': line.price_unit,
+            }))
+
+        # Create the Sale Order
+        # opportunity_id link ensures it appears in the existing smart button
+        sale_order = self.env['sale.order'].create({
+            'partner_id': self.partner_id.id,
+            'opportunity_id': self.id,
+            'team_id': self.team_id.id,
+            'campaign_id': self.campaign_id.id,
+            'medium_id': self.medium_id.id,
+            'source_id': self.source_id.id,
+            'order_line': order_lines,
+        })
+
+        return {
+            'name': _('Quotation'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'sale.order',
+            'view_mode': 'form',
+            'res_id': sale_order.id,
+            'target': 'current',
+        }
